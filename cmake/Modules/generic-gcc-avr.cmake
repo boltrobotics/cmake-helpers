2f86933e078c1582ee0bea18487c987c7b937823
Sergey Kapustin <svkapustin@gmail.com>
2019-02-21 21:15:03 -0500

    Reformat

--- cmake/Modules/generic-gcc-avr.cmake
+++ cmake/Modules/generic-gcc-avr.cmake
@@ -298,8 +298,8 @@ endfunction(avr_target_link_libraries EXECUTABLE_TARGET)
 ##########################################################################
 
 function(avr_target_include_directories EXECUTABLE_TARGET)
-    if(NOT ARGN)
-        message(FATAL_ERROR "No include directories to add to ${EXECUTABLE_TARGET}.")
+    if (NOT ARGN)
+      message(FATAL_ERROR "No include directories to add to ${EXECUTABLE_TARGET}.")
     endif()
 
     get_target_property(TARGET_LIST ${EXECUTABLE_TARGET} OUTPUT_NAME)
@@ -315,8 +315,8 @@ endfunction()
 ##########################################################################
 
 function(avr_target_compile_definitions EXECUTABLE_TARGET)
-    if(NOT ARGN)
-        message(FATAL_ERROR "No compile definitions to add to ${EXECUTABLE_TARGET}.")
+    if (NOT ARGN)
+      message(FATAL_ERROR "No compile definitions to add to ${EXECUTABLE_TARGET}.")
     endif()
 
     get_target_property(TARGET_LIST ${EXECUTABLE_TARGET} OUTPUT_NAME)
@@ -325,47 +325,51 @@ function(avr_target_compile_definitions EXECUTABLE_TARGET)
    target_compile_definitions(${TARGET_LIST} ${extra_args})
 endfunction()
 
+##########################################################################
+# Get/set operating parameters 
+##########################################################################
+
 function(avr_generate_fixed_targets)
-   # get status
-   add_custom_target(
-      get_status
-      ${AVR_UPLOADTOOL} ${AVR_UPLOADTOOL_BASE_OPTIONS} -P ${AVR_UPLOADTOOL_PORT} -n -v
-      COMMENT "Get status from ${AVR_MCU}"
-   )
+  # get status
+  add_custom_target(
+    get_status
+    ${AVR_UPLOADTOOL} ${AVR_UPLOADTOOL_BASE_OPTIONS} -P ${AVR_UPLOADTOOL_PORT} -n -v
+    COMMENT "Get status from ${AVR_MCU}"
+  )
    
-   # get fuses
-   add_custom_target(
-      get_fuses
-      ${AVR_UPLOADTOOL} ${AVR_UPLOADTOOL_BASE_OPTIONS} -P ${AVR_UPLOADTOOL_PORT} -n
-         -U lfuse:r:-:b
-         -U hfuse:r:-:b
-         -U efuse:r:-:b
-      COMMENT "Get fuses from ${AVR_MCU}"
-   )
-   
-   # set fuses
-   add_custom_target(
-      set_fuses
+  # get fuses
+  add_custom_target(
+    get_fuses
+    ${AVR_UPLOADTOOL} ${AVR_UPLOADTOOL_BASE_OPTIONS} -P ${AVR_UPLOADTOOL_PORT} -n
+      -U lfuse:r:-:b
+      -U hfuse:r:-:b
+      -U efuse:r:-:b
+    COMMENT "Get fuses from ${AVR_MCU}"
+  )
+  
+  # set fuses
+  add_custom_target(
+    set_fuses
+    ${AVR_UPLOADTOOL} ${AVR_UPLOADTOOL_BASE_OPTIONS} -P ${AVR_UPLOADTOOL_PORT}
+      -U lfuse:w:${AVR_L_FUSE}:m
+      -U hfuse:w:${AVR_H_FUSE}:m
+      -U efuse:w:${AVR_E_FUSE}:m
+    COMMENT "Setup: High Fuse: ${AVR_H_FUSE} Low Fuse: ${AVR_L_FUSE} Extended Fuse: ${AVR_E_FUSE}"
+  )
+  
+  # get oscillator calibration
+  add_custom_target(
+    get_calibration
       ${AVR_UPLOADTOOL} ${AVR_UPLOADTOOL_BASE_OPTIONS} -P ${AVR_UPLOADTOOL_PORT}
-         -U lfuse:w:${AVR_L_FUSE}:m
-         -U hfuse:w:${AVR_H_FUSE}:m
-         -U efuse:w:${AVR_E_FUSE}:m
-         COMMENT "Setup: High Fuse: ${AVR_H_FUSE} Low Fuse: ${AVR_L_FUSE} Extended Fuse: ${AVR_E_FUSE}"
-   )
-   
-   # get oscillator calibration
-   add_custom_target(
-      get_calibration
-         ${AVR_UPLOADTOOL} ${AVR_UPLOADTOOL_BASE_OPTIONS} -P ${AVR_UPLOADTOOL_PORT}
-         -U calibration:r:${AVR_MCU}_calib.tmp:r
-         COMMENT "Write calibration status of internal oscillator to ${AVR_MCU}_calib.tmp."
-   )
-   
-   # set oscillator calibration
-   add_custom_target(
-      set_calibration
-      ${AVR_UPLOADTOOL} ${AVR_UPLOADTOOL_BASE_OPTIONS} -P ${AVR_UPLOADTOOL_PORT}
-         -U calibration:w:${AVR_MCU}_calib.hex
-         COMMENT "Program calibration status of internal oscillator from ${AVR_MCU}_calib.hex."
-   )
+      -U calibration:r:${AVR_MCU}_calib.tmp:r
+    COMMENT "Write calibration status of internal oscillator to ${AVR_MCU}_calib.tmp."
+  )
+  
+  # set oscillator calibration
+  add_custom_target(
+    set_calibration
+    ${AVR_UPLOADTOOL} ${AVR_UPLOADTOOL_BASE_OPTIONS} -P ${AVR_UPLOADTOOL_PORT}
+      -U calibration:w:${AVR_MCU}_calib.hex
+    COMMENT "Program calibration status of internal oscillator from ${AVR_MCU}_calib.hex."
+  )
 endfunction()
