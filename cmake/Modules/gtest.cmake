ada01de81b086bc7739290bc635f8239c9f4ed35
Sergey Kapustin <svkapustin@gmail.com>
2019-04-26 11:04:52 -0400

    Configure gtest target. Update make.sh. FORCE_UPDATE libopencm3
    - Turned off gmock build
    - Changed output path for gtest, gtest_main
    - Added environment configuration of dependent variables to make.sh
    - Removed LIB_TYPE from example

--- cmake/Modules/gtest.cmake
+++ cmake/Modules/gtest.cmake
@@ -14,6 +14,8 @@ else ()
 
   # Prevent overriding the parent project's compiler/linker settings on Windows
   set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+  option(BUILD_GMOCK OFF)
+  option(INSTALL_GTEST OFF)
 
   add_project(
     PREFIX gtest
@@ -21,4 +23,20 @@ else ()
     HOME "${GTEST_HOME}"
     INC_DIR "${GTEST_HOME}/googletest/include")
 
+  if (TARGET gtest)
+    set_target_properties(gtest
+      PROPERTIES
+      RUNTIME_OUTPUT_DIRECTORY "${OUTPUT_PATH}/bin"
+      LIBRARY_OUTPUT_DIRECTORY "${OUTPUT_PATH}/lib"
+      ARCHIVE_OUTPUT_DIRECTORY "${OUTPUT_PATH}/lib"
+      PDB_OUTPUT_DIRECTORY "${OUTPUT_PATH}/bin")
+  endif ()
+  if (TARGET gtest_main)
+    set_target_properties(gtest_main
+      PROPERTIES
+      RUNTIME_OUTPUT_DIRECTORY "${OUTPUT_PATH}/bin"
+      LIBRARY_OUTPUT_DIRECTORY "${OUTPUT_PATH}/lib"
+      ARCHIVE_OUTPUT_DIRECTORY "${OUTPUT_PATH}/lib"
+      PDB_OUTPUT_DIRECTORY "${OUTPUT_PATH}/bin")
+  endif ()
 endif ()
--- cmake/Modules/libopencm3.cmake
+++ cmake/Modules/libopencm3.cmake
@@ -7,9 +7,9 @@ add_project(
   PREFIX libopencm3
   HOME "${LIBOPENCM3_HOME}"
   URL "https://github.com/libopencm3/libopencm3.git"
-  BUILD_CMD "make TARGETS=stm32/${STM32_FAMILY_LOWER} VERBOSE=1"
+  BUILD_CMD "make TARGETS=stm32/${STM32_FAMILY_LOWER}"
   BUILD_IN 1
-  FORCE_UPDATE 0
+  FORCE_UPDATE 1
   LIB_DIR "${LIBOPENCM3_HOME}/lib"
   LIB_NAME opencm3_stm32${STM32_FAMILY_LOWER})
 
