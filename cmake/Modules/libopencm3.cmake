154bf4c403db30b56e3a98ee501cb18a9ad9752c
Sergey Kapustin <svkapustin@gmail.com>
2019-05-22 13:26:32 -0400

    Add flag to enable/disable libopencm3 update
    - Added the check of environemtn variable FORCE_UPDATE

--- cmake/Modules/libopencm3.cmake
+++ cmake/Modules/libopencm3.cmake
@@ -3,6 +3,12 @@ include(project_setup)
 set(LIBOPENCM3_HOME $ENV{LIBOPENCM3_HOME})
 string(TOLOWER ${STM32_FAMILY} STM32_FAMILY_LOWER)
 
+if (DEFINED ENV{FORCE_UPDATE})
+  set(FORCE_UPDATE $ENV{FORCE_UPDATE})
+else ()
+  set(FORCE_UPDATE 0)
+endif ()
+
 add_project(
   PREFIX libopencm3
   HOME "${LIBOPENCM3_HOME}"
@@ -10,7 +16,7 @@ add_project(
   URL "https://github.com/libopencm3/libopencm3.git"
   BUILD_CMD "make TARGETS=stm32/${STM32_FAMILY_LOWER}"
   BUILD_IN 1
-  FORCE_UPDATE 1
+  FORCE_UPDATE ${FORCE_UPDATE}
   LIB_DIR "${LIBOPENCM3_HOME}/lib"
   LIB_NAME opencm3_stm32${STM32_FAMILY_LOWER})
 
